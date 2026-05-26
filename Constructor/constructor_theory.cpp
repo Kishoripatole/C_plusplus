ca4097f141b9492b1c1ee284459ebb0a45d4b9ae
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-26 22:44:45 +0530

    automatically_called_when_an_object_is_created
    Added constructor message and clarified constructor behavior.

--- Constructor/constructor_theory.cpp
+++ Constructor/constructor_theory.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 using namespace std;
 
-class Textbook{
+class Textbook{ //Base class
     public:
     string edition;
     string subject;
     string publish_date;
 
+    // Constructor
     Textbook(){
         cout<<"Hello , I am constructor!";
         cout<<"This is textbook edition !";
     }
 
+   //Methods
     void display(){
         cout<<"Edition :"<<edition<<endl;
     }
@@ -20,7 +22,7 @@ class Textbook{
 
 int main()
 {
-    Textbook Tb1;//
+    Textbook Tb1;// Constructors are automatically called when an object is created
     Textbook Tb2;
 
     Tb1.edition="\"02/02/1906\"";

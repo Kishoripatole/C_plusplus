e325faa34a5b4c375dae65468fd8144a454df280
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-30 16:13:23 +0000

    30/5/26

---
+++ Polymorphism/Function_overloading/number_of_parameters.cpp
@@ -0,0 +1,27 @@
+ #include<iostream>
+using namespace std;
+
+void printinfo(int age)
+{
+    cout<<"Age:"<<age<<endl;
+
+}
+
+void printinfo(int age,float heigth)
+{
+    cout<<"Age:"<<age<<endl<<"Height:"<<heigth<<endl;
+}
+
+void printinfo(int age,float heigth,string name)
+{
+    cout<<"Age:"<<age<<endl<<"height:"<<heigth<<endl<<"Name:"<<name<<endl;
+}
+
+int main()
+{
+    printinfo(19);
+    cout<<"--------"<<endl;
+    printinfo(20,157);
+    cout<<"--------"<<endl;
+    printinfo(21,158,"kishori");
+}

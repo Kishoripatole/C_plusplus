8371619cd83cc0a8d7225ec735ea978b73c99b4a
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-30 16:07:01 +0000

    30/5/26

---
+++ Polymorphism/function_overloading.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Cat{
+    public:
+
+    void show()
+    {
+        cout<<"eating bread"<<endl;
+    }
+
+};
+
+class Dog:public Cat{
+    public:
+
+    void show()
+    {
+        cout<<"eating bread and milk"<<endl;
+    }
+};
+
+int main()
+{
+    Dog d;
+    d.show();
+
+}
